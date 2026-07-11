e0caa0a657ded4240c40a12e84e134d8ab690bb9
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-07-11 19:46:00 +0800

    added basic ordering logic

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-import express from "express";
+import express from "express"; // Web server framework
+import ejs from "ejs"; // Template engine used to render .html views
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(import.meta.dirname));
+
+app.engine(".html", ejs.renderFile); // Render .html files using the EJS engine
+app.set("view engine", "html"); // Default view file extension for res.render()
+
+app.use(express.urlencoded({ extended: true })); // This allows us to read POST request bodies
+app.use(express.static(".")); // Serves index.html (and any other root static files) automatically
 
 app.post("/", (req, res) => {
 	console.log(req.body);
-	res.send("Order received");
+
+	// Insert logic to store it to the database
+	// Get the database instance
+
+	res.render("Confirmation", req.body); 
 });
 
-app.listen(3000);
+// Start server
+app.listen(3000, () => console.log("Open the application at http://localhost:3000"));
